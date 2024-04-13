d69e5daf4bfd9a50dd0f14d0fff49292116c92c8
olayemiabdul <olayemi.abdullahi@gmail.com>
2024-04-13 20:48:34 +0100

    3rd commit

--- lib/our_work.dart
+++ lib/our_work.dart
@@ -24,77 +24,79 @@ class _OurWorkPageState extends State<OurWorkPage> {
   Widget build(BuildContext context) {
     final isDesktop = Responsive.isDesktop(context);
 
-    return Scaffold(
-      appBar: !isDesktop
-          ? AppBar(
-        backgroundColor: Colors.black,
-        title:  Padding(
-          padding: const EdgeInsets.only(left: 100),
-          child: InkWell(
-            onTap: () {},
-            child: Container(
-              padding: const EdgeInsets.only(left: 3, top: 3),
-              height: 35,
-              width: 220,
-              decoration: BoxDecoration(
-                borderRadius: const BorderRadius.only(
-                    topLeft: Radius.circular(10),
-                    topRight: Radius.circular(10),
-                    bottomLeft: Radius.circular(10),
-                    bottomRight: Radius.circular(10)),
-                color: Colors.green,
-                border: Border.all(
-                  color: Colors.white,
-                  width: 1,
+    return SafeArea(
+      child: Scaffold(
+        appBar: !isDesktop
+            ? AppBar(
+          backgroundColor: Colors.black,
+          title:  Padding(
+            padding: const EdgeInsets.only(left: 100),
+            child: InkWell(
+              onTap: () {},
+              child: Container(
+                padding: const EdgeInsets.only(left: 3, top: 3),
+                height: 35,
+                width: 220,
+                decoration: BoxDecoration(
+                  borderRadius: const BorderRadius.only(
+                      topLeft: Radius.circular(10),
+                      topRight: Radius.circular(10),
+                      bottomLeft: Radius.circular(10),
+                      bottomRight: Radius.circular(10)),
+                  color: Colors.green,
+                  border: Border.all(
+                    color: Colors.white,
+                    width: 1,
+                  ),
                 ),
-              ),
-              child: RichText(
-                text: const TextSpan(
-                  text: 'WhiteSnow',
-                  style: TextStyle(
-                      fontFamily: 'ConcertOne',
-                      color: Colors.white,
-                      fontSize: 20),
-                  children: <TextSpan>[
-                    TextSpan(
-                      text: ' Garden care',
-                      style: TextStyle(
-                          fontFamily: 'ConcertOne',
-                          color: Colors.white,
-                          fontSize: 20),
-                    ),
-                  ],
+                child: RichText(
+                  text: const TextSpan(
+                    text: 'WhiteSnow',
+                    style: TextStyle(
+                        fontFamily: 'ConcertOne',
+                        color: Colors.white,
+                        fontSize: 20),
+                    children: <TextSpan>[
+                      TextSpan(
+                        text: ' Garden care',
+                        style: TextStyle(
+                            fontFamily: 'ConcertOne',
+                            color: Colors.white,
+                            fontSize: 20),
+                      ),
+                    ],
+                  ),
                 ),
               ),
             ),
           ),
-        ),
-      )
-          : null,
-      endDrawer: !isDesktop
-          ? SizedBox(
-        width: 250,
-
-        child: isDesktop ? const AppbarContainer(color: Colors.red,) : const DrawerWidget(),)
-          : null,
-      body:
-
-          Container(
-            color: Colors.amber,
-            height: MediaQuery.of(context).size.height,
-            width: MediaQuery.of(context).size.width,
-            child: Column(
-              children: [
-                if (!Responsive.isMobile(context)) const AppbarContainer(),
-
-                const CarouselContainer(),
-                const LandScapeDesign(),
-                const BottomWidget()
-              ],
+        )
+            : null,
+        endDrawer: !isDesktop
+            ? SizedBox(
+          width: 250,
+      
+          child: isDesktop ? const AppbarContainer(color: Colors.red,) : const DrawerWidget(),)
+            : null,
+        body:
+      
+            Container(
+              color: Colors.amber,
+              height: MediaQuery.of(context).size.height,
+              width: MediaQuery.of(context).size.width,
+              child: Column(
+                children: [
+                  if (!Responsive.isMobile(context)) const AppbarContainer(),
+      
+                  const CarouselContainer(),
+                  const LandScapeDesign(),
+                  const BottomWidget()
+                ],
+              ),
             ),
-          ),
-
-
+      
+      
+      ),
     );
   }
 }
